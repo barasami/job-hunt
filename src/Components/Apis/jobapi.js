3d46d5fc80a2b4e1f868052b62c9311c9384c16a
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-21 22:37:20 +0300

    fixing

---
+++ src/Components/Apis/jobapi.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const Url='https://remote-jobs-api.p.rapidapi.com/jobs'
+
+const options = {
+  params: {company: 'shopify'},
+  headers: {
+    'X-RapidAPI-Key': '87f0c01e35msh8962c83c8aef89ap1e9713jsnbf3fe9f2f818',
+    'X-RapidAPI-Host': 'remote-jobs-api.p.rapidapi.com'
+  }
+};
+
+export const myApi=async()=>{
+    try {
+	    const response = await axios.get(Url,options);
+	    return response;
+    } 
+    catch (error) {
+	console.log(error);
+    }
+}
---
+++ src/Components/Homejob/Homejob.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { myApi } from '../Apis/jobapi'
+
+function Homejob() {
+  return (
+    <div>Homejob</div>
+  )
+}
+
+export default Homejob
