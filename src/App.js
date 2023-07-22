4dd1b58586b9e16e1a971ce0077f70b6a872ad03
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-22 22:07:04 +0300

    fixing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Homejob from './Components/Homejob/Homejob';
+import Homelatest from './Components/Latestjob/Homelatest';
+import Homeresume from './Components/Resumes/Homeresume';
+import Homeletter from './Letter/Homeletter';
 
 function App() {
   return (
     <div className="App">
+      <Homelatest/>
       <Homejob/>
+      <Homeresume/>
+      <Homeletter/>
     </div>
   );
 }
---
+++ src/Letter/Homeletter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { allLetter } from './letter'
+
+function Homeletter() {
+  return (
+    <div>Homeletter</div>
+  )
+}
+
+export default Homeletter
