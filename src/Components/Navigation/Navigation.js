19689b4418347d2904f3bc7c2b960b242d19ad10
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-22 22:35:13 +0300

    updated facets

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import HomeIcon from '@mui/icons-material/Home';//home
+import ContentPasteSearchIcon from '@mui/icons-material/ContentPasteSearch';//search job
+import WorkIcon from '@mui/icons-material/Work';//resume
+import EditCalendarIcon from '@mui/icons-material/EditCalendar';//letter
+import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
