4aa23324dc8b728c79a870c0675f9499e0d21269
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-22 22:22:35 +0300

    added Nav

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Homejob from './Components/Homejob/Homejob';
 import Homelatest from './Components/Latestjob/Homelatest';
 import Homeresume from './Components/Resumes/Homeresume';
 import Homeletter from './Letter/Homeletter';
+import Footer from './Components/Footer/Footer';
+import Navigation from './Components/Navigation/Navigation';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
       <Fragment>
+        <Navigation/>
         <Routes>
           <Route path='/' element={<Homelatest/>}/>
           <Route path='/search' element={<Homejob/>}/>
           <Route path='/resume' element={<Homeresume/>}/>
           <Route path='/letter' element={<Homeletter/>}/>
         </Routes>
+        <Footer/>
       </Fragment>
     </Router>
   );
