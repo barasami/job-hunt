7f156aee49772f768550c2bdd0210af378094fa3
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-22 22:16:20 +0300

    fixing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import { Fragment } from 'react';
 import './App.css';
 import Homejob from './Components/Homejob/Homejob';
 import Homelatest from './Components/Latestjob/Homelatest';
 import Homeresume from './Components/Resumes/Homeresume';
 import Homeletter from './Letter/Homeletter';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Homelatest/>
-      <Homejob/>
-      <Homeresume/>
-      <Homeletter/>
-    </div>
+    <Router>
+      <Fragment>
+        <Routes>
+          <Route path='/' element={<Homelatest/>}/>
+          <Route path='/search' element={<Homejob/>}/>
+          <Route path='/resume' element={<Homeresume/>}/>
+          <Route path='/letter' element={<Homeletter/>}/>
+        </Routes>
+      </Fragment>
+    </Router>
   );
 }
 
