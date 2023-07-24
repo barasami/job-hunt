2fc250fa8c29f8a9b59de43fd9695c2ef54b33bd
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-24 22:13:57 +0300

    updated page

--- src/Components/Homejob/Homejob.js
+++ src/Components/Homejob/Homejob.js
@@ -13,11 +13,23 @@ function Homejob() {
     })
   },[])
 
-  let allData=job.data
-  console.log(allData);
+  let jobNews=job.items
+  let allJob=jobNews?.map(({tittle,location,company_name,company_logo,date,id})=>{
+    return(
+      <div key={id}>
+        <div>{tittle}</div>
+        <div>{company_logo}</div>
+        <div>{company_name}</div>
+        <div>{date}</div>
+        <div>{location}</div>
+      </div>
+    )
+  })
   return (
     <div className='homejob'>
-      Homejob
+      <div>
+        {allJob}
+      </div>
     </div>
   )
 }
