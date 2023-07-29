c48a2b89771500daa2174a9580e2d6ac398785f6
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-29 23:02:28 +0300

    updated

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import './Navigation.css'
+import WorkIcon from '@mui/icons-material/Work';
 
 function Navigation() {
   return (
     <div className='nav'>
         <div className='navigation'>
+          <div className='links'><WorkIcon color='secondary'/></div>
           <div className='links'><Link className='link' to='/'>Latest</Link></div>
           <div className='links'><Link  className='link'to='/search'>Shopify</Link></div>
         </div>
